0d55bbccb374aa0f83fa5499a318ebe27976f197
terifan <pinjata69@gmail.com>
2016-12-04 19:29:22 +0100

    update

--- src/org/terifan/algebra/Vec2d.java
+++ src/org/terifan/algebra/Vec2d.java
@@ -1,7 +1,7 @@
 package org.terifan.algebra;
 
 
-public class Vec2d 
+public class Vec2d implements Cloneable
 {
 	public double x;
 	public double y;
@@ -34,9 +34,61 @@ public class Vec2d
 	}
 
 
+	public Vec2d add(double x, double y)
+	{
+		this.x += x;
+		this.y += y;
+
+		return this;
+	}
+
+
+	public Vec2d add(double aValue)
+	{
+		this.x += aValue;
+		this.y += aValue;
+
+		return this;
+	}
+
+
+	public Vec2d scale(double aScale)
+	{
+		x *= aScale;
+		y *= aScale;
+
+		return this;
+	}
+
+
+	public Vec2d scale(double aScaleX, double aScaleY)
+	{
+		x *= aScaleX;
+		y *= aScaleY;
+
+		return this;
+	}
+
+
+	public Vec2d scale(Vec2d aVector)
+	{
+		x *= aVector.x;
+		y *= aVector.y;
+
+		return this;
+	}
+
+
 	@Override
 	public String toString()
 	{
 		return "Vec2f{" + "x=" + x + ", y=" + y + '}';
+	}
+
+
+	@Override
+	public Vec2d clone()
+	{
+		return new Vec2d(x, y);
 	}
 }
